dffe27b4b4ec42f7b8e94a65d41154158bcf9009
But <buttaiwan@gmail.com>
2021-09-11 09:25:19 +0800

    v1.2 支援雙色

--- ShowGuideSheets.glyphsReporter/Contents/Resources/plugin.py
+++ ShowGuideSheets.glyphsReporter/Contents/Resources/plugin.py
@@ -57,12 +57,11 @@ class ShowGuideSheets(ReporterPlugin):
 		# show guides
 		try:
 			#color = NSColor.colorWithRed_green_blue_alpha_(0.1, 0.6, 0.8, 0.35)
-			color = self.getColor(0.35)
-			color.set()
-
+			self.getColor(1, 0.35).set()
 			closedPaths.setLineWidth_(1.0 / scale)
 			closedPaths.stroke()
 
+			self.getColor(0, 0.35).set()
 			opendPath.setLineWidth_(1.0 / scale)
 			opendPath.stroke()
 
@@ -73,7 +72,7 @@ class ShowGuideSheets(ReporterPlugin):
 					self.drawTextAtPoint(
 							ann.text, ann.position, 
 							fontSize = 10, 
-							fontColor = color,
+							fontColor = self.getColor(0, 0.35),
 							align = "topleft",
 						)
 		
@@ -87,8 +86,8 @@ class ShowGuideSheets(ReporterPlugin):
 			try:
 				for path in layer.paths:
 					for nodes in path.nodes:
-						if closedPaths.isStrokeHitByPoint_padding_(nodes.position, .6): self.drawOnPathPoint(nodes.position, scale)
-						if opendPath.isStrokeHitByPoint_padding_(nodes.position, .6): self.drawOnPathPoint(nodes.position, scale)
+						if closedPaths.isStrokeHitByPoint_padding_(nodes.position, .6): self.drawOnPathPoint(nodes.position, self.getColor(1, 0.25), scale)
+						if opendPath.isStrokeHitByPoint_padding_(nodes.position, .6): self.drawOnPathPoint(nodes.position, self.getColor(0, 0.25), scale)
 			
 			except Exception as e:
 				self.logToConsole( "showGuideSheetNodes: %s\n" % str(e) )
@@ -96,10 +95,11 @@ class ShowGuideSheets(ReporterPlugin):
 				print(traceback.format_exc())
 
 	@objc.python_method
-	def drawOnPathPoint(self, pos, scale):
+	def drawOnPathPoint(self, pos, color, scale):
 		r = RADIUS * 1.0 / scale
 		#NSColor.colorWithRed_green_blue_alpha_(0.1, 0.6, 0.8, 0.25).set()
-		self.getColor(0.25).set()
+		#self.getColor(0.25).set()
+		color.set()
 		rect = NSRect(NSPoint(pos.x-r, pos.y-r), NSSize(r*2, r*2))
 		circle = NSBezierPath.bezierPathWithRoundedRect_cornerRadius_(rect, r)
 		circle.fill()
@@ -109,13 +109,18 @@ class ShowGuideSheets(ReporterPlugin):
 		circle.stroke()
 
 	@objc.python_method
-	def getColor(self, alpha):
+	def getColor(self, index, alpha):
 		colorHex = Glyphs.font.customParameters["Guide Color"]
 
 		try:
-			r = int(colorHex[0:2], 16) / 256.0
-			g = int(colorHex[2:4], 16) / 256.0
-			b = int(colorHex[4:6], 16) / 256.0
+			if index == 1 and len(colorHex) >= 13:
+				r = int(colorHex[7:9], 16) / 255.0
+				g = int(colorHex[9:11], 16) / 255.0
+				b = int(colorHex[11:13], 16) / 255.0
+			else:
+				r = int(colorHex[0:2], 16) / 255.0
+				g = int(colorHex[2:4], 16) / 255.0
+				b = int(colorHex[4:6], 16) / 255.0
 			return NSColor.colorWithRed_green_blue_alpha_(r, g, b, alpha)
 		except:
 			return NSColor.colorWithRed_green_blue_alpha_(0.1, 0.6, 0.8, alpha)
